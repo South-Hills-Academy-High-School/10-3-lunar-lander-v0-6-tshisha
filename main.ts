a218c9bd509c13a611e153dafb46dbe0b2d0de57
tshisha <113388366+tshisha@users.noreply.github.com>
2022-10-03 13:30:43 -0700

    fix the game over

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     angle += 15 * (3.14 / 180)
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthWest1, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileDarkGrass3, function (sprite, location) {
     if (Math.abs(apple.vx) > 20 || apple.vy > 50) {
         boom = sprites.create(img`
             . . . . 2 2 2 2 2 2 2 2 . . . . 
@@ -31,11 +31,10 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthWest1, fun
             . . 2 2 4 4 4 4 4 4 4 4 2 2 . . 
             . . . 2 2 4 4 4 4 4 2 2 2 . . . 
             `, SpriteKind.Projectile)
-        boom.setPosition(scene.cameraProperty(CameraProperty.X), scene.cameraProperty(CameraProperty.Y))
+        boom.setPosition(apple.x, apple.y)
         scene.cameraFollowSprite(boom)
         music.bigCrash.playUntilDone()
-        pause(1000)
-        game.over(false)
+        Gameoverflag += 1
     } else {
         if (landingFlag == 0) {
             fuel += 100
@@ -58,6 +57,8 @@ let angle = 0
 let fireball: Sprite = null
 let landingFlag = 0
 let rocketflag = 0
+tiles.setCurrentTilemap(tilemap`level3`)
+let Gameoverflag = 0
 let fuel = 1000
 let fuelSprite = textsprite.create(convertToText(fuel))
 rocketflag = 0
@@ -82,7 +83,6 @@ fireball = sprites.create(img`
     `, SpriteKind.Projectile)
 fireball.setFlag(SpriteFlag.Invisible, true)
 angle = 0
-tiles.setCurrentTilemap(tilemap`level2`)
 effects.clouds.startScreenEffect()
 apple = sprites.create(img`
     . . . . . . . e c 7 . . . . . . 
@@ -127,5 +127,11 @@ game.onUpdate(function () {
     }
     if (apple.y < 150) {
         landingFlag = 0
+    }
+    if (Gameoverflag == 1) {
+        game.over(false)
+    }
+    if (Gameoverflag == 1) {
+        Gameoverflag += 1
     }
 })
